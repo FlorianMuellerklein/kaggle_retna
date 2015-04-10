7708fd812e1b615ef11bdfa47b0f4bf7eef380bb
Florian <f.muellerklein@gmail.com>
2015-04-09 21:21:48 -0400

    Update trim_images2.sh

--- trim_images2.sh
+++ trim_images2.sh
@@ -4,7 +4,7 @@ IMAGES="/Volumes/Mildred/Kaggle/Retna/Data/Backup/train/*.jpeg"
 for file in $IMAGES
 do
 	echo "$file"
-	convert $file -fuzz 10% -trim +repage -resize 196x196 -gravity center -background black -extent 196x196 $file
+	convert $file -fuzz 10% -trim +repage -resize 512x512 -gravity center -background black -extent 512x512 $file
 	
 done
 
